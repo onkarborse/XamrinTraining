777478ef0bb19bbbb0ccf62d5a21948327db4295
oborse <onkar.borse@dxc.com>
2024-02-14 15:48:42 +0530

    Xamrin Demo

---
+++ MyFirstApp/MyFirstApp/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Xamarin.Forms;
+
+namespace MyFirstApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        //void Button_Clicked(System.Object sender, System.EventArgs e)
+        //{
+        //}
+
+        //void Button_Clicked_1(System.Object sender, System.EventArgs e)
+        //{
+        //    DisplayAlert("display","now","ok");
+        //}
+        //private void Button_Clicked_1(object sender, EventArgs e)
+        //{
+        //    label1.Text = "Hello Xamarin";
+        //}
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            label1.Text = fName.Text + " " + lName.Text;
+        }
+    }
+}
+
---
+++ MyFirstApp/MyFirstApp/MyNewPage.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+using Xamarin.Forms.PlatformConfiguration.iOSSpecific;
+
+namespace MyFirstApp
+{	
+	public partial class MyNewPage : ContentPage
+	{	
+		//public MyNewPage ()
+		//{
+		//	InitializeComponent ();
+		//}
+  //      private void Button_Clicked_1(object sender, EventArgs e)
+  //      {
+  //          if (sender == "admin")
+  //          {
+  //              DisplayAlert("Login success", "Okay", "Cancel");
+  //          } else
+  //          {
+  //              DisplayAlert("Login failed", "Okay", "Cancel");
+  //          }
+  //      }
+
+  //      private void Button_Clicked_2(object sender, EventArgs e)
+  //      {
+  //          label1.Text = fName.Text + " " + lName.Text;
+  //      }
+  //      void Button_Clicked(System.Object sender, System.EventArgs e)
+  //      {
+  //      }
+
+        public MyNewPage()
+        {
+            InitializeComponent();
+            
+          //  label1.SetValue = 0;
+            //Content = new Label
+            //{
+            //    Text = "Hello now render via code behind...",
+            //    HorizontalOptions = LayoutOptions.Center,
+            //    VerticalOptions = LayoutOptions.Center
+            //};
+        }
+
+        //void fName_TextChanged(System.Object sender, Xamarin.Forms.TextChangedEventArgs e)
+        //{
+        //    label1.Text = e.NewTextValue;
+        //}
+        //private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+        //{
+        //    label1.Text = string.Format("Value is {0:F2}", e.NewValue);
+        //}
+        //void Slider_ValueChanged(System.Object sender, Xamarin.Forms.ValueChangedEventArgs e)
+        //{
+        //}
+
+        //void Slider_ValueChanged_1(System.Object sender, Xamarin.Forms.ValueChangedEventArgs e)
+        //{
+        //}
+    }
+
+}
+
